e72932fd7a2ecc70cf6f3aea50cbc5677d29f1b4
Yana <sieghostienko@gmail.com>
2023-06-09 14:22:09 +0300

    task50

---
+++ task50/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+int row = ReadInt("Введите индекс строки: ");
+int col = ReadInt("Введите индекс столбца: ");
+int[,] numbers = new int[6, 8];
+FillArray2D(numbers);
+PrintArray2D(numbers);
+
+if (row < numbers.GetLength(0) && col < numbers.GetLength(1)) Console.WriteLine(numbers[row, col]);
+else Console.WriteLine($"{row}{col} -> такого числа в массиве нет");
+
+void FillArray2D(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(0); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+void PrintArray2D(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+             System.Console.Write(array[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
